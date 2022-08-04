438a900d57ed24cca5e0fabae0eb5c0509f99f75
Worawut Aunkham <jameskater350@gmail.com>
2022-08-04 20:34:25 +0700

    1.6

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,23 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    edit() {
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+      this.GStore.flashMessage = 'The data has been update ' + this.event.title
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+    }
+  }
 }
 </script>
